18a0fcba66c648a08eb2a3782d557a75007f1733
jasmitha1474 <jasmithapnair@gmail.com>
2026-03-17 09:35:40 +0530

    [Jasmitha] added Use Case 2: Basic Room Types & Static Availability

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,11 +1,40 @@
-//Use Case 1: Application Entry & Welcome Message
-
 import static java.lang.IO.println;
+//Use Case 2: Basic Room Types & Static Availability
 
-public class BookMyStayApp {
-    public static void main(String[] args) {
-        println("welcome to the hotel booking management system");
-        println("system initialised sucessfully");
+abstract class Room {
+
+    // Attributes
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    // Constructor
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
     }
 
+    // Method to display details
+    public void displayRoomDetails() {
+        System.out.println("Number of Beds: " + numberOfBeds);
+        System.out.println("Room Size (sq ft): " + squareFeet);
+        System.out.println("Price per Night: " + pricePerNight);
+    }
+}
+
+// Child class
+class DeluxeRoom extends Room {
+
+    public DeluxeRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+// Main class
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        Room room = new DeluxeRoom();
+        room.displayRoomDetails();
+    }
 }
